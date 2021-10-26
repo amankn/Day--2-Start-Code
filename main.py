34a22e919f43f93935d6bb47c378d1fcbe107268
Aman Juneja <71624646+amankn@users.noreply.github.com>
2021-10-26 16:02:01 +0000

    Learning Integers

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ print("Aman"[3])
 print("123" + "345")
 
 print("12" + "18")
+
+#Integers
+
+print(123 + 345)
+
+# How to put commas in large Integers - python we use underscores
+print(3_45_000)
+
+
+
